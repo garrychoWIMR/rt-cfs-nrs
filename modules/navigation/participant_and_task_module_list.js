cd6b0aa1aa631945aa587ae9f1937fe94c5a75b9
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-01-11 09:24:11 +1100

    update

---
+++ modules/navigation/participant_and_task_module_list.js
@@ -0,0 +1,121 @@
+L[A+'participant']=                 {table_id:'20010378',var:v,url:B+'/modules/participant/participant.html'};
+L[A+'notes']=                       {table_id:'20010379',var:v,url:B+'/modules/task_notes/notes.html'};
+L[A+'site_filter']=                 {table_id:'20010382',var:v,url:B+'/modules/participant/site_filter.html'};
+L[A+'participant_login_setup']=     {table_id:'20010380',var:v,url:B+'/modules/participant/participant_login_setup.html'};
+
+L[A+'Medical_history']={table_id:'20010385',var:v,url:B+'/modules/task/medical_history.html',notes:' Medical history', form_module:A+ 'Medical_history_form'};
+L[A+'Medical_history_form']={table_id:'20010385', var:v,url:B+'/modules/task/medical_history.form.html'};
+
+L[A+'Adverse_Event']={table_id:'20010389',var:v,url:B+'/modules/task/Adverse_Event.html',notes:' Adverse Event'};
+L[A+'Concom_Medication']={table_id:'20010390',var:v,url:B+'/modules/task/concom_medication.html',notes:'Concomitant Medication',form_module:A+'Concom_Medication_Form'};
+L[A+'Concom_Medication_Form']={table_id:'20010390',var:v,url:B+ '/modules/task/concom_medication.form.html'};
+L[A+'File_Notes']={table_id:'20010386',var:v,url:B+'/modules/task/File_Notes.html',notes:'File Notes'};
+
+
+//Screening
+L[A+'Screening_Visitdate']={table_id:'20010391',var:v,url:B+'/modules/task/Date_of_Visit.html',notes:'Screening Visit Date'};
+L[A+'Screening_Anthro']={table_id:'20010392',var:v,url:B+'/modules/task/Anthro.html',notes:'Screening Anthropometry'};
+L[A+'Screening_Actigraphy']={table_id:'20010393',var:v,url:B+'/modules/task/Actigraphy.html',notes:'Screening Actigraphy '};
+L[A+'Screening_Sleepdiary']={table_id:'20010394',var:v,url:B+'/modules/task/File_upload.html',notes:'Screening Sleep Diary'};
+L[A+'Screening_DiagnosticPSG']={table_id:'20010395',var:v,url:B+'/modules/task/PSG300.html',notes:'Diagnostic Sleep Report'};
+L[A+'Screening_EDF']={table_id:'20010396',var:v,url:B+'/modules/task/EDF_File_upload.html',notes:'EDF file upload'};
+L[A+'Screening_Sleepscoring']={table_id:'20010397',var:v,url:B+'/modules/task/File_upload.html',notes:'Sleep scoring file upload'};
+L[A+'Screening_IncluExclu']={table_id:'20010398',var:v,url:B+'/modules/task/IncluExclu.html',notes:'Inclusion Exclusion Criteria', form_module:A+'Screening_IncluExclu_form'};
+L[A+'Screening_IncluExclu_form']={table_id:'20010398',var:v,url:B+'/modules/task/IncluExclu.form.html'};
+
+
+//Screening Questionnaires
+L[A+'Screening_ESS']={table_id:'20010403',var:v,url:B+'/modules/task/Epworth_Sleepiness_Scale.html',notes:'Screening Epworth Sleepiness Scale (ESS)',form_module:A+'Screening_ESS_form',task:'Screening Epworth Sleepiness Scale (ESS)'};
+L[A+'Screening_ESS_form']={table_id:'20010403',var:v,url:B+'/modules/task/Epworth_Sleepiness_Scale_form.html'};
+L[A+'Screening_MFSI']={table_id:'20010399',var:v,url:B+'/modules/task/mfsi.html',notes:'Screening Multidimensional Fatigue Symptom Inventory',form_module:A+'Screening_MFSI_form',task:'Screening Multidimensional Fatigue Symptom Inventory (MFSI-SF)'};
+L[A+'Screening_MFSI_form']={table_id:'20010399',var:v,url:B+'/modules/task/mfsi_form.html'};
+L[A+'Screening_PSQI']={table_id:'20010400',var:v,url:B+'/modules/task/psqi.html',notes:'Screening Pittsburgh Sleep Quality Index (PSQI)', form_module: A+ 'Screening_PSQI_Form',task:'Screening Pittsburgh Sleep Quality Index (PSQI)'};
+L[A+'Screening_PSQI_Form']={table_id:'20010400',var:v,url:B+'/modules/task/psqi.form.html'};
+L[A+'Screening_DASS']={table_id:'20010401',var:v,url:B+'/modules/task/dass.html',notes: 'Screening DASS', form_module:A+ 'Screening_DASS_form', task: 'Screening Depression Anxiety and Stress Scale (DASS-21)'};
+L[A+'Screening_DASS_form']={table_id:'20010401',var:v,url:B+'/modules/task/dass.form.html'};
+L[A+'Screening_Chalder']={table_id:'20010402',var:v,url:B+'/modules/task/chalder.html',notes: 'Screening Chalder Fatigue Scale', form_module:A+'Screening_Chalder_form', task: 'Screening Chalder Fatigue Scale'};
+L[A+'Screening_Chalder_form']={table_id:'20010402',var:v,url:B+'/modules/task/chalder_form.html'};
+L[A+'Screening_Canadian']={table_id:'20010404',var:v,url:B+'/modules/task/Canadian.html',notes: 'Screening Canadian Consensus Severity Scale', form_module:A+ 'Screening_Canadian_form', task: 'Screening Canadian Consensus Symptom Severity Scale'};
+L[A+'Screening_Canadian_form']={table_id:'20010404',var:v,url:B+'/modules/task/Canadian_form.html'};
+L[A+'Screening_SFH']={table_id:'20010405',var:v,url:B+'/modules/task/short_form_survey_instrument.html',notes: 'Screening Short form Health Survey', form_module:A+ 'Screening_SFH_form', task: 'Screening Short Form Health Survey (SF-36)'};
+L[A+'Screening_SFH_form']={table_id:'20010405',var:v,url:B+'/modules/task/short_form_survey_instrument.form.html'};
+
+
+//Night 1 - High Density Night
+L[A+'Night_Visit_Date']={table_id:'20010406',var:v,url:B+'/modules/task/Date_of_Visit.html',notes:'Baseline Visit Date'};
+L[A+'Night_Visit_Anthro']={table_id:'20010407',var:v,url:B+'/modules/task/Anthro.html',notes:'Baseline Anthropometry'};
+L[A+'Night_Visit_Flinders']={table_id:'20010408',var:v,url:B+'/modules/task/Flinders.html',notes:'Night Flinders Fatigue Scale',form_module:A+'Night_Visit_Flinders_form',task:'Flinders Fatigue Scale'};
+L[A+'Night_Visit_Flinders_form']={table_id:'20010408',var:v,url:B+'/modules/task/Flinders_form.html'};
+L[A+'NightVisit_EDF']={table_id:'20010410',var:v,url:B+'/modules/task/EDF_File_upload.html',notes:'EDF file upload'};
+L[A+'Night_Visit_Sleepscoring']={table_id:'20010409',var:v,url:B+'/modules/task/File_upload.html',notes:'Sleep scoring file upload'};
+
+
+L[A+'Mental_Fatigue_One']={table_id:'20011125',var:v,url:B+'/modules/task/Mentalscale.html',notes:'Mental Fatigue Scale 1.0',form_module:A+'Mental_Fatigue_One_form',task:'Mental Fatigue Scale 1'};
+L[A+'Mental_Fatigue_One_form']={table_id:'20011125',var:v,url:B+'/modules/task/Mentalscale.form.html'};
+L[A+'Mental_Fatigue_Two']={table_id:'20011126',var:v,url:B+'/modules/task/Mentalscale.html',notes:'Mental Fatigue Scale 1.1',form_module:A+'Mental_Fatigue_Two_form',task:'Mental Fatigue Scale 2'};
+L[A+'Mental_Fatigue_Two_form']={table_id:'20011126',var:v,url:B+'/modules/task/Mentalscale.form.html'};
+L[A+'Mental_Fatigue_Three']={table_id:'20011127',var:v,url:B+'/modules/task/Mentalscale.html',notes:'Mental Fatigue Scale 1.2',form_module:A+'Mental_Fatigue_Three_form',task:'Mental Fatigue Scale 3'};
+L[A+'Mental_Fatigue_Three_form']={table_id:'20011127',var:v,url:B+'/modules/task/Mentalscale.form.html'};
+L[A+'Mental_Fatigue_Four']={table_id:'20011128',var:v,url:B+'/modules/task/Mentalscale.html',notes:'Mental Fatigue Scale 1.3',form_module:A+'Mental_Fatigue_Four_form',task:'Mental Fatigue Scale 4'};
+L[A+'Mental_Fatigue_Four_form']={table_id:'20011128',var:v,url:B+'/modules/task/Mentalscale.form.html'};
+L[A+'Mental_Fatigue_Five']={table_id:'20011130',var:v,url:B+'/modules/task/Mentalscale.html',notes:'Mental Fatigue Scale 1.4',form_module:A+'Mental_Fatigue_Five_form',task:'Mental Fatigue Scale 5'};
+L[A+'Mental_Fatigue_Five_form']={table_id:'20011130',var:v,url:B+'/modules/task/Mentalscale.form.html'};
+L[A+'Mental_Fatigue_six']={table_id:'20011305',var:v,url:B+'/modules/task/Mentalscale.html',notes:'Mental Fatigue Scale 2.0',form_module:A+'Mental_Fatigue_six_form',task:'Mental Fatigue Scale 6'};
+L[A+'Mental_Fatigue_six_form']={table_id:'20011305',var:v,url:B+'/modules/task/Mentalscale.form.html'};
+L[A+'Mental_Fatigue_seven']={table_id:'20011306',var:v,url:B+'/modules/task/Mentalscale.html',notes:'Mental Fatigue Scale 2.1',form_module:A+'Mental_Fatigue_seven_form',task:'Mental Fatigue Scale 7'};
+L[A+'Mental_Fatigue_seven_form']={table_id:'20011306',var:v,url:B+'/modules/task/Mentalscale.form.html'};
+
+
+
+//L[A+'NightVisit__EDF']={table_id:'--------',var:v,url:B+'/modules/task/EDF_File_upload.html',notes:'EDF file upload'};
+
+//Neurobehavioural tests
+L[A+'NightVisit_ERP']={table_id:'20010411',var:v,url:B+'/modules/task/File_upload.html',notes:'ERP file upload'};
+//L[A+'NightVisit_KDT']={table_id:'--------',var:v,url:B+'/modules/task/KDT.html',notes:'Karolinska Drowsiness tests (KDT)'};
+
+
+L[A+'NightVisit_Memorytask_Fileupload']={table_id:'20011129',var:v,url:B+'/modules/task/Memtest.html',notes:'Evening Memory task tests'};
+
+
+//Night 1 - Morning
+L[A+'Morning_visit_KDT']={table_id:'20010413',var:v,url:B+'/modules/task/KDT.html',notes:'Baseline  Karolinska Drowsiness Test (KDT)'};
+L[A+'Morning_visit_word_pair_Set_1']={table_id:'--------',var:v,url:B+'/modules/task/morning_word_pair_set_1.html',notes:'Baseline Morning Word Pair 1', task: 'Morning Word Pair 1'};
+L[A+'Morning_visit_word_pair_Set_2']={table_id:'--------',var:v,url:B+'/modules/task/morning_word_pair_set_2.html',notes:'Baseline Morning Word Pair 2', task: 'Morning Word Pair 2'};
+L[A+'Morning_visit_LDT_Test_Set_1']={table_id:'--------',var:v,url:B+'/modules/task/LDT_Morning_Set1.html', notes: 'Baseline Morning Lexical Decision Task 1', task: 'Morning Lexical Decision Task 1'};
+L[A+'Morning_visit_LDT_Test_Set_2']={table_id:'--------',var:v,url:B+'/modules/task/LDT_Morning_Set2.html', notes: 'Baseline Morning Lexical Decision Task 2', task: 'Morning Lexical Decision Task 2'};
+L[A+'Morning_LCT']={table_id:'20010416',var:v,url:B+'/modules/task/LCT.html',notes:'Morning Letter Cancellation Task',form_module:A+'Morning_LCT_form',task:'Morning Letter cancellation Task'};
+L[A+'Morning_LCT_Form']={table_id:'20010416',var:v,url:B+'/modules/task/LCT_form.html'};
+L[A+'Morning_ERP']={table_id:'20010415',var:v,url:B+'/modules/task/ERP.html',notes:'Morning Event Related Potential',form_module:A+'Morning_ERP',task:'Morning Event Related Potential'};
+L[A+'Morning_PASAT']={table_id:'20010417',var:v,url:B+'/modules/task/PASAT.html',notes:'Morning Paced Auditory Serial Addition Test',form_module:A+'Morning_PASAT',task:'Morning Paced Auditory Serial Addition Tests (PASAT)'};
+L[A+'Morning_PASAT_Form']={table_id:'20010417',var:v,url:B+'/modules/task/PASAT_form.html'};
+//L[A+'Morning_KDT']={table_id:'--------',var:v,url:B+'/modules/task/KDT.html',notes:'Baseline  Karolinska Drowsiness Test (KDT)'};
+L[A+'Morning_BloodSaliva']={table_id:'20010419',var:v,url:B+'/modules/task/Bloodsaliva.html',notes:'Morning Blood and Saliva Samples'};
+L[A+'Morning_MRI']={table_id:'20010420',var:v,url:B+'/modules/task/MRI.html',notes:'Magnetic Resonance Imagining (MRI)'};
+L[A+'Morning_KDT']={table_id:'--------',var:v,url:B+'/modules/task/KDT.html',notes:'Morning End Karolinska Drowsiness Test (KDT)'};
+
+
+//Memory Tasks
+
+L[A+'Prac_Evening_Wordpairs_Set1']={table_id:'20009358',var:v,url:B+'/modules/task/Set1_Evening_Wordpairs_Practice.html',notes:'Memory task - Word pairs Set 1', task: 'Evening Practice Word Pair 1'};
+L[A+'Prac_Evening_Wordpairs_Set2']={table_id:'20009358',var:v,url:B+'/modules/task/Set2_evening_wordpairs_Practice.html',notes:'Memory task - Word pairs Set 1', task: 'Evening Practice Word Pair 2'};
+L[A+'wordpairs1t1']={table_id:'20009358',var:v,url:B+'/modules/task/Set1_Evening_Wordpairs.html',notes:'Memory task - Word pairs Set 1', task: 'Evening Word Pair 1'};
+L[A+'wordpairs2t1']={table_id:'20009358',var:v,url:B+'/modules/task/Set2_evening_wordpairs.html',notes:'Memory task - Word pairs Set 2', task: 'Evening Word Pair 1'};
+L[A+'wordpairs1t2']={table_id:'20009358',var:v,url:B+'/modules/task/Set1_Evening_Wordpairs.html',notes:'Memory task - Word pairs Set 1', task: 'Evening Word Pair 1'};
+L[A+'wordpairs2t2']={table_id:'20009358',var:v,url:B+'/modules/task/Set2_evening_wordpairs.html',notes:'Memory task - Word pairs Set 2', task: 'Evening Word Pair 1'};
+L[A+'wordpairs1t3']={table_id:'20009358',var:v,url:B+'/modules/task/Set1_Evening_Wordpairs.html',notes:'Memory task - Word pairs Set 1', task: 'Evening Word Pair 1'};
+L[A+'wordpairs1t4']={table_id:'20009358',var:v,url:B+'/modules/task/Set1_Evening_Wordpairs.html',notes:'Memory task - Word pairs Set 1', task: 'Evening Word Pair 1'};
+L[A+'wordpairs2t3']={table_id:'20009358',var:v,url:B+'/modules/task/Set2_evening_wordpairs.html',notes:'Memory task - Word pairs Set 2', task: 'Evening Word Pair 1'};
+L[A+'wordpairs2t4']={table_id:'20009358',var:v,url:B+'/modules/task/Set2_evening_wordpairs.html',notes:'Memory task - Word pairs Set 1', task: 'Evening Word Pair 1'};
+L[A+'Morning_word_pair_Set_1']={table_id:'20009358',var:v,url:B+'/modules/task/Set1_Morning_Wordpairs.html',notes:'Memory task - Word pairs Set 1', task: 'Morning Word Pair 1'};
+L[A+'Morning_word_pair_Set_2']={table_id:'20009358',var:v,url:B+'/modules/task/Set2_morning_wordpairs.html',notes:'Memory task - Word pairs Set 1', task: 'Morning Word Pair 1'};
+
+
+//KSS
+
+L[A+'KSS1']={table_id:'20011302',var:v,url:B+'/modules/task/kss.html',notes: 'Karolinska Sleepiness Scale (KSS)', form_module:A+ 'kss_Quest1', task: 'Karolinska Sleepiness Scale 1.0'};
+L[A+'kss_Quest1']={table_id:'20011302',var:v,url:B+'/modules/task/kss.QUEST.html'};
+L[A+'KSS2']={table_id:'20011303',var:v,url:B+'/modules/task/kss.html',notes: 'Karolinska Sleepiness Scale (KSS)', form_module:A+ 'kss_Quest2', task: 'Karolinska Sleepiness Scale 1.0'};
+L[A+'kss_Quest2']={table_id:'20011303',var:v,url:B+'/modules/task/kss.QUEST.html'};
+L[A+'KSS3']={table_id:'20011304',var:v,url:B+'/modules/task/kss.html',notes: 'Karolinska Sleepiness Scale (KSS)', form_module:A+ 'kss_Quest3', task: 'Karolinska Sleepiness Scale 1.0'};
+L[A+'kss_Quest3']={table_id:'20011304',var:v,url:B+'/modules/task/kss.QUEST.html'};
+L[A+'Morning_Memorytask_Fileupload']={table_id:'20011341',var:v,url:B+'/modules/task/File_upload.html'};
---
+++ modules/navigation/quest_module_list.js
@@ -0,0 +1,26 @@
+var A="woolcock_CFS_NRS_";
+var list={
+
+    "Screening":[
+        A+'Screening_ESS',
+        A+'Screening_MFSI',
+        A+'Screening_PSQI',
+        A+'Screening_DASS',
+        A+'Screening_Chalder',
+        A+'Screening_SFH',
+        A+'Screening_Canadian',
+
+    ],
+
+    "Baseline Evening Questionnaires":[
+      A+'Night_Visit_Flinders',
+    ],
+
+    "Mental Fatigue Scales":[
+      A+'Mental_Fatigue_One',
+      A+'Mental_Fatigue_Two',
+      A+'Mental_Fatigue_Three',
+      A+'Mental_Fatigue_Four',
+      A+'Mental_Fatigue_Five',
+],
+};
